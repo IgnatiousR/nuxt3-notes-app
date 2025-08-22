30a9b978e66f3f791b455bd2c02a22b0805b50d1
Ignatious Rozario <ignatiousr80@gmail.com>
2025-08-22 22:00:10 +0600

    Create test.vue

---
+++ pages/test.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from "vue";
+
+const show = ref(true);
+</script>
+
+<template>
+  <div v-auto-animate class="bg-black p-8 inline-block overflow-hidden">
+    <button
+      class="px-4 py-2 bg-yellow-500 rounded-md text-black font-bold"
+      @click="show = !show"
+    >
+      Toggle
+    </button>
+
+    <!-- width animates here -->
+    <div
+      :class="[show ? 'w-[400px]' : 'w-0']"
+      class="mt-4 bg-black text-white p-4 rounded-lg overflow-hidden transition-all duration-500"
+    >
+      Animated div 🚀
+    </div>
+  </div>
+</template>
