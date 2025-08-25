1f2c06a5afaa7f8e20dc6daa62573ae1481bb394
Ignatious Rozario <ignatiousr80@gmail.com>
2025-08-25 21:48:32 +0600

    Update schema.ts

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,34 +1,30 @@
 // import { integer, pgTable, serial, text, timestamp, varchar } from 'drizzle-orm/pg-core';
-import * as t from 'drizzle-orm/pg-core';
+import * as t from "drizzle-orm/pg-core";
 
-export const usersTable = t.pgTable('users', {
-  id: t.integer().primaryKey().generatedAlwaysAsIdentity(),
+export const usersTable = t.pgTable("users", {
+  id: t.serial().primaryKey(),
   name: t.varchar({ length: 255 }).notNull(),
   email: t.varchar({ length: 255 }).notNull().unique(),
   password: t.varchar({ length: 255 }).notNull(),
   salt: t.varchar({ length: 255 }).notNull(),
+  createdAt: t.timestamp("created_at").notNull().defaultNow(),
+  updatedAt: t
+    .timestamp("updated_at")
+    .notNull()
+    .$onUpdate(() => new Date()),
 });
 
-// export const usersTable = pgTable('users_table', {
-//   id: serial('id').primaryKey(),
-//   name: text('name').notNull(),
-//   age: integer('age').notNull(),
-//   email: text('email').notNull().unique(),
-//   password: text('password').notNull(),
-//   salt: text('password').notNull(),
-// });
-
-export const notesTable = t.pgTable('notes', {
-  id: t.serial('id').primaryKey(),
-  title: t.varchar('title', { length: 255 }).notNull(),
-  content: t.text('content').notNull(),
+export const notesTable = t.pgTable("notes", {
+  id: t.serial("id").primaryKey(),
+  title: t.varchar("title", { length: 255 }).notNull(),
+  content: t.text("content").notNull(),
   userId: t
-    .integer('user_id')
+    .integer("user_id")
     .notNull()
-    .references(() => usersTable.id, { onDelete: 'cascade' }),
-  createdAt: t.timestamp('created_at').notNull().defaultNow(),
+    .references(() => usersTable.id, { onDelete: "cascade" }),
+  createdAt: t.timestamp("created_at").notNull().defaultNow(),
   updatedAt: t
-    .timestamp('updated_at')
+    .timestamp("updated_at")
     .notNull()
     .$onUpdate(() => new Date()),
 });
