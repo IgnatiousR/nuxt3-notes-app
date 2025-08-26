2e2e0befb814358693c3e6f55e33d6127766d6ce
Ignatious Rozario <ignatiousr80@gmail.com>
2025-08-26 22:27:07 +0600

    Update delete.ts

--- src/queries/delete.ts
+++ src/queries/delete.ts
@@ -1,7 +1,13 @@
 import { eq } from "drizzle-orm";
-import { usersTable, type SelectUser } from "../db/schema";
+import { notesTable, usersTable, type SelectNote, type SelectUser } from "../db/schema";
 import { db } from "../db/db";
 
 export async function deleteUser(id: SelectUser["id"]) {
   await db.delete(usersTable).where(eq(usersTable.id, id));
+}
+
+export async function deleteNote(id: SelectNote["id"]) {
+  const [deletedNote] = await db.delete(notesTable).where(eq(notesTable.id, id)).returning();
+
+  return deletedNote ?? null;
 }
