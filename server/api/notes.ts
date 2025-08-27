d5331e1360fe2bcfc3ba48b97db91535a0a0035b
Ignatious Rozario <ignatiousr80@gmail.com>
2025-08-27 11:19:44 +0600

    Update notes.ts

--- server/api/notes.ts
+++ server/api/notes.ts
@@ -1,14 +1,19 @@
-import { getNotesByUserId } from "~/src/queries/select";
+import { getNotesByUserId } from '~/src/queries/select';
 
 export default defineEventHandler(async (_event) => {
   try {
-    // const notes = await db
-    //   .select()
-    //   .from(notesTable)
-    //   .where(eq(notesTable.userId, userId))
-    //   .orderBy(notesTable.createdAt);
+    // const config = useRuntimeConfig();
+    // const jwtCookie = useCookie('NotesJWT', { _event });
+    // console.log(jwtCookie.value);
+    // const jwtCookie = useCookie('NotesJWT');
+    // if (jwtCookie.value) {
+    //   const decoded = jwt.verify(jwtCookie.value, config.jwtSecret);
+    //   const userId = decoded.id;
+    //   console.log(decoded.id);
+    // }
+
     const notes = await getNotesByUserId(1);
-    console.log("Notes:", notes);
+    // console.log('Notes:', notes);
 
     return notes;
   } catch (err) {
