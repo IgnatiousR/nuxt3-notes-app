ba4bff367eaa437cd2e2c09dea5a8771a35e4948
Ignatious Rozario <ignatiousr80@gmail.com>
2025-08-27 11:19:49 +0600

    Update update.ts

--- src/queries/update.ts
+++ src/queries/update.ts
@@ -1,18 +1,19 @@
-import { eq } from "drizzle-orm";
-import { notesTable, usersTable, type InsertNote, type SelectNote, type SelectUser } from "../db/schema";
-import { db } from "../db/db";
+import { eq } from 'drizzle-orm';
+import { notesTable, usersTable, type InsertNote, type SelectNote, type SelectUser } from '../db/schema';
+import { db } from '../db/db';
 
-export async function updateUser(id: SelectUser["id"], data: Partial<Omit<SelectUser, "id">>) {
+export async function updateUser(id: SelectUser['id'], data: Partial<Omit<SelectUser, 'id'>>) {
   const [updatedUser] = await db.update(usersTable).set(data).where(eq(usersTable.id, id)).returning();
-  return updatedUser ?? null;
+  // return updatedUser ?? null;
+  return Object.keys(updatedUser).length > 0 ? updatedUser : null;
 }
 
-export async function updateNote(id: SelectNote["id"], data: Partial<Omit<InsertNote, "id" | "userId">>) {
+export async function updateNote(id: SelectNote['id'], data: Partial<Omit<InsertNote, 'id' | 'userId'>>) {
   const [updatedNote] = await db
     .update(notesTable)
     .set({ ...data, updatedAt: new Date() })
     .where(eq(notesTable.id, id))
     .returning();
-
-  return updatedNote ?? null;
+  return Object.keys(updatedNote).length > 0 ? updatedNote : null;
+  // return updatedNote ?? null;
 }
