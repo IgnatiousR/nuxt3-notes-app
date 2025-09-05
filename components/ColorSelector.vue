2f327cc4f4805f5994abeacdf3c405f5dbe09851
Ignatious Rozario <ignatiousr80@gmail.com>
2025-09-05 16:28:13 +0600

    Update ColorSelector.vue

--- components/ColorSelector.vue
+++ components/ColorSelector.vue
@@ -1,15 +1,22 @@
 <template>
   <ClientOnly v-if="!colorMode?.forced">
-    <Button variant="outline" @click="isDark = !isDark">
+    <!-- <Button variant="outline" class="cursor-pointer border-0 bg-transparent" @click="isDark = !isDark">
       <Moon v-if="isDark" class="w-6 h-6" />
       <Sun v-if="!isDark" class="w-6 h-6" />
-    </Button>
+    </Button> -->
+    <button
+      class="dark:text-zinc-400 text-zinc-600 hover:cursor-pointer hover:dark:text-white hover:text-black"
+      @click="isDark = !isDark"
+    >
+      <Moon v-if="isDark" class="w-5 h-5" />
+      <Sun v-if="!isDark" class="w-5 h-5" />
+    </button>
   </ClientOnly>
 </template>
 
 <script setup lang="ts">
 // import { Icon } from "@iconify/vue";
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 import { Moon, Sun } from "lucide-vue-next";
 
 const colorMode = useColorMode();
