6d74bbc835630d0ad1b029a595568f6ef02823bb
Ignatious Rozario <ignatiousr80@gmail.com>
2025-09-07 16:24:59 +0600

    Create loading.vue

---
+++ pages/loading.vue
@@ -0,0 +1,10 @@
+<template>
+    <section className="pb-40 pt-10 bg-white">
+      <div className="w-full">
+        <div className="flex flex-col justify-center items-center h-screen">
+          <!-- <div className="spinner-border mb-3" role="status"></div> -->
+          <h1 className="text-center text-4xl">Loading...</h1>
+        </div>
+      </div>
+    </section>
+</template>
