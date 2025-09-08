89701e43842daf09e178bbfd1708bd2e27655ae4
Ignatious Rozario <ignatiousr80@gmail.com>
2025-09-08 12:24:20 +0600

    Added web analytics from vercel

--- app.vue
+++ app.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt';
+import { Toaster } from "@/components/ui/sonner";
+</script>
+ 
 <template>
+  <Analytics />
+  <Toaster rich-colors theme="dark" position="top-center" :expand="true" />
   <NuxtPage />
 </template>
